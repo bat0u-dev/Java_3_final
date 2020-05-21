6bb3decedeb908c4f210f8b07d92dd8f689b61cd
bat0u-dev <batou7891@gmail.com>
2020-05-21 19:41:56 +0300

    Выполнено задание к lesson1 Java#3

--- src/Lesson1_Generics/main/java/Box.java
+++ src/Lesson1_Generics/main/java/Box.java
@@ -1,2 +1,30 @@
-public class Box {
+import java.util.ArrayList;
+//test
+
+public class Box<T extends Fruit> {
+    T fruit;
+
+    ArrayList<T> list = new ArrayList<>();
+
+    public void put(T fruit) {
+        list.add(fruit);
+    }
+
+    public double getBoxWeight() {
+        double weight;
+        weight = list.size() * (double) list.get(0).getWeight();
+        return weight;
+    }
+
+    public boolean compare(Box<?> box){
+        return this.getBoxWeight() == box.getBoxWeight();
+    }
+
+    public void shift(Box<? super T> box){ //так и не понял, почему тут не может быть просто Box<T>?
+        box.list.clear();
+        box.list.trimToSize();
+        box.list.addAll(this.list);
+        this.list.clear();
+        this.list.trimToSize();
+    }
 }
